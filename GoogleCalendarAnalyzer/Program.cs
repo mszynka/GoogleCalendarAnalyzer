3c1765fc909c6445774da3a64055e44a4b32a6a8
Mateusz Szynka <mateusz.szynka@mediporta.pl>
2016-12-05 13:21:00 +0100

    Initial commit

---
+++ GoogleCalendarAnalyzer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Linq;
+using Ical.Net;
+
+namespace GoogleCalendarAnalyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = Directory.GetFiles("../../Data/").Where(x => x.EndsWith(".ics")).ToList();
+            foreach (var file in files)
+            {
+                var calendarCollection = Calendar.LoadFromFile(file);
+                var firstCalendar = calendarCollection.First();
+                var firstEvent = firstCalendar.Events.First();
+            }
+        }
+    }
+}
